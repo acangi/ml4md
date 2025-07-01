dad21b104d0ebd1c28aea571806ac75e1ff8bd1e
Attila Cangi <a.cangi@hzdr.de>
2025-07-01 18:17:40 +0200

    Fixed special character issue in output yaml file

--- scripts/openalex_to_yaml.py
+++ scripts/openalex_to_yaml.py
@@ -99,7 +99,9 @@ def write_yaml_files(records: List[Dict[str, Any]]):
     ]:
         with path.open("w", encoding="utf-8") as f:
             f.write(header)
-            yaml.safe_dump(data, f, allow_unicode=True, sort_keys=False)
+            yaml.safe_dump(
+                data, f, allow_unicode=True, sort_keys=False, default_style='"'
+            )
         print(f"Wrote {len(data)} {name} to {path}")
 
 
